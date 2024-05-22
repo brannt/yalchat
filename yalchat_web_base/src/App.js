b43bc5cbf6eddea163fa1a47c7605c3a3f1f4ed4
Artem Gorokhov <branntart@gmail.com>
2024-05-22 21:09:20 +0200

    web_base: Rebrand, lol

--- yalchat_web_base/src/App.js
+++ yalchat_web_base/src/App.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import ChatInterface from "./components/ChatInterface";
 import LoginForm from "./components/LoginForm";
+import "./App.css";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App container is-flex is-flex-direction-column">
-      <section className="hero is-small is-info">
+      <section className="hero is-small">
         <div className="hero-body">
           <div className="container">
-            <h1 className="title">Yet Another LLM Chat App</h1>
-            <h2 className="subtitle">A humble chatbot</h2>
+            <h1 className="title">
+              Helpful Chat, <span className="lol">lol</span>
+            </h1>
+            <h2 className="subtitle">by brannt</h2>
           </div>
         </div>
       </section>
